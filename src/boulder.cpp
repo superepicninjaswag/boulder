eab7fb9cd5b2a985e5c664ccdb2641fbfeef5270
Rajvir Samrai <samrai.rajvir@gmail.com>
2023-10-07 13:02:52 -0400

    Add boilerplate SDL setup and fix CMake rpath for OSX 14 update

--- src/boulder.cpp
+++ src/boulder.cpp
@@ -1,5 +1,55 @@
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
 int main(int argc, char* argv[]) {
+    if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Boulder",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SCREEN_WIDTH,
+                                          SCREEN_HEIGHT,
+                                          0);
+    if (window == NULL) {
+        SDL_Log("Unable to create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                                -1,
+                                                SDL_RENDERER_SOFTWARE);
+    if (renderer == NULL) {
+        SDL_Log("Unable to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Hardware-accelerated Rendering: Draw a red diagonal line
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderDrawLine(renderer, 0, 0, 800, 600);
+    SDL_RenderPresent(renderer);
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        SDL_Delay(10);  // Just to prevent 100% CPU usage
+    }
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
