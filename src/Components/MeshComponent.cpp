172ef8ca51e66271c08f7d144ffde769d3408b83
Rajvir Samrai <samrai.rajvir@gmail.com>
2025-02-22 16:59:37 -0500

    Remove tutorial regurgitation
    I was following javidx9's "3D Graphics Engine" series on YouTube.
    Good stuff but I want to write something that feels more like my own
    work. Yes yes, everyone stands on the shoulders of giants and there
    is nothing new under the sun. Whatever.

--- src/Components/MeshComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "MeshComponent.h"
-
-MeshComponent::MeshComponent(int meshResourceId)
-{
-    this->meshResourceId = meshResourceId;
-}
--- src/Game.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <SDL2/SDL.h>
-
-#include "Boulder.h"
-#include "./ECS/ECS.h"
-#include "./Rendering/Rendering.h"
-#include "./Rendering/Renderer.h"
